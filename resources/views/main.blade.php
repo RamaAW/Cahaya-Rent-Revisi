da5fca85f7e3a9ce955486bafa206f02e0e71673
RamaAW <ramawibi123@gmail.com>
2021-11-30 08:53:34 +0700

    tes kelima

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,15 +6,15 @@
     <title>Cahaya Rent | Admin</title>
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" href="{{asset('img/logo2.png')}}">
-    <link rel="shortcut icon" href="{{asset('img/logo2.png')}}">
+    <link rel="apple-touch-icon" href="img/logo2.png">
+    <link rel="shortcut icon" href="img/logo2.png">
     <link rel="stylesheet" href="style/assets/css/normalize.css">
     <link rel="stylesheet" href="style/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('style/assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/assets/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/assets/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/assets/css/cs-skin-elastic.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/assets/scss/style.css') }}">
+    <link rel="stylesheet" href="style/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="style/assets/css/flag-icon.min.css">
+    <link rel="stylesheet" href="style/assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="style/assets/scss/style.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -101,7 +101,7 @@
                 <div class="col-sm-5">
                     <div class="user-area dropdown float-right">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="{{ asset('style/images/admin.jpg') }}">
+                            <img class="user-avatar rounded-circle" src="style/images/admin.jpg">
                         </a>
                         <div class="user-menu dropdown-menu">
                             <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
@@ -141,9 +141,9 @@
         
 
     </div>    
-    <script src="{{ asset('style/assets/js/vendor/jquery-2.1.4.min.js') }}"></script>
-    <script src="{{ asset('style/assets/js/popper.min.js') }}"></script>
-    <script src="{{ asset('style/assets/js/plugins.js') }}"></script>
-    <script src="{{ asset('style/assets/js/main.js') }}"></script>
+    <script src="style/assets/js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="style/assets/js/popper.min.js"></script>
+    <script src="style/assets/js/plugins.js"></script>
+    <script src="style/assets/js/main.js"></script>
 </body>
 </html>
